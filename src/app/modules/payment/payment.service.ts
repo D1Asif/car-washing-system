1d1d93e25dd7d2f4b0a9df26860e8a17017179b3
Dewan Asifur Rahman <dasifrahman@gmail.com>
2024-10-16 20:16:29 +0600

    try catch block added in payment verification

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -1,34 +1,41 @@
+import httpStatus from "http-status";
+import AppError from "../../errors/AppError";
 import { Booking } from "../booking/booking.model";
 import { verifyPayment } from "./payment.util"
 import { readFileSync } from "fs"
 import { join } from "path"
 
 const confirmPaymentIntoDB = async (query: Record<string, unknown>) => {
-    const { transactionId, status, bookingId } = query;
+    try {
+        const { transactionId, status, bookingId } = query;
 
-    const verificationResponse = await verifyPayment(transactionId as string);
+        const verificationResponse = await verifyPayment(transactionId as string);
 
-    let result;
-    let message;
+        let result;
+        let message;
 
-    if (verificationResponse && verificationResponse.pay_status === 'Successful' && status === 'success') {
-        result = await Booking.findByIdAndUpdate(
-            bookingId,
-            { paymentStatus: 'paid' },
-            { new: true }
-        )
+        if (verificationResponse && verificationResponse.pay_status === 'Successful' && status === 'success') {
+            result = await Booking.findByIdAndUpdate(
+                bookingId,
+                { paymentStatus: 'paid' },
+                { new: true }
+            )
 
-        message = "Payment Successful!"
-    } else {
-        message = "Payment failed!"
+            message = "Payment Successful!"
+        } else {
+            message = "Payment failed!"
+        }
+
+        const filePath = join(__dirname, "./confirmation.html")
+        let template = readFileSync(filePath, 'utf-8')
+
+        template = template.replace('{{msg}}', message);
+
+        return template;
+    } catch (err) {
+        throw new AppError(httpStatus.INTERNAL_SERVER_ERROR, "Payment verification failure")
     }
 
-    const filePath = join(__dirname, "./confirmation.html")
-    let template = readFileSync(filePath, 'utf-8')
-
-    template = template.replace('{{msg}}', message);
-
-    return template;
 }
 
 export const PaymentServices = {
